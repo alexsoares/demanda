c0da0fc83b0a53d8a7275d920690ffc1b26ff317
tt <alexandrejr_soares@yahoo.com.br>
2010-05-05 09:35:36 -0300

    Teste 7

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,4 +27,10 @@ after "deploy:symlink", "deploy:update_crontab"
  end
 
 
+  namespace :deploy do
+    desc "Update the crontab file"
+    task :update_crontab, :roles => :db do
+      run "cd #{release_path} && whenever --update-crontab #{application}"
+    end
+  end
 
